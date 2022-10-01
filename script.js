e3176aa01c031f274544b2a765f0b38c80bc3566
Leksa <admin@petiteweb.ru>
2022-10-01 20:07:40 +0300

    add click event

--- script.js
+++ script.js
@@ -2,4 +2,13 @@ const body = document.body;
 const square = body.querySelector(".square");
 const link = square.querySelector(".btn");
 
-square.style.backgroundColor = getColor(30, 0, 256, 256);
+square.style.backgroundColor = getColor(30, 0, 200);
+
+function changeBg(el) {
+    el.style.backgroundColor = getColor(70, 256, 0, 256);
+}
+let n = 0;
+square.onclick = function() {
+    n++;
+    this.innerHTML = link.outerHTML + `Clicked ${n} times`;
+}
